a1b6837e2aaec05206a2925435db6d6a09bcaa39
Geo <13597665+github-34@users.noreply.github.com>
2022-04-09 11:50:31 -0400

    Initial Commit

---
+++ src/LaracacheServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Insomnicles\Laracache;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaracacheServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'insomnicles');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'insomnicles');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/laracache.php', 'laracache');
+
+        // Register the service the package provides.
+        $this->app->singleton('laracache', function ($app) {
+            return new Laracache;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['laracache'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/laracache.php' => config_path('laracache.php'),
+        ], 'laracache.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/insomnicles'),
+        ], 'laracache.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/insomnicles'),
+        ], 'laracache.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/insomnicles'),
+        ], 'laracache.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
