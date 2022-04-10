43ab532c97412c82b0c53ee0c080bf2be202bd9d
Geo <13597665+github-34@users.noreply.github.com>
2022-04-10 12:59:40 -0400

    Added allinCache to Cachable

--- src/Cachable.php
+++ src/Cachable.php
@@ -6,8 +6,25 @@ trait Cachable
 {
     // Build wonderful things
 
-    public function hello() 
+    public function hello()
     {
 	dd('hello');
+    }
+    public static function allInCache() : Collection
+    {
+        $reflectionClass = new \ReflectionClass(self::class);
+        $cacheKey = $reflectionClass->getShortName();
+
+        $id = 1;
+        $models = new Collection();
+        while(1) {
+            $modelStr = Cache::get($cacheKey.":".$id);
+            if (is_null($modelStr))
+                break;
+            $model = unserialize($modelStr);
+            $models->put($model->id, $model);
+            $id++;
+        }
+        return $models;
     }
 }
